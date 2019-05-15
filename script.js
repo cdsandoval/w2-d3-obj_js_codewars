530ad6647ab4c4b89de59d9736216cc61eb4f2ad
Carlos Sandoval <iznomag@gmail.com>
2019-05-15 12:05:12 -0500

    Add first exercises solution

---
+++ script.js
@@ -0,0 +1,21 @@
+//Super Class Extension
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    return this.name + " makes a noise, ";
+  }
+}
+
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+  }
+
+  speak() {
+    return super.speak() + this.name + " goes meow.";
+  }
+}
