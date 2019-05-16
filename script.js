1eb0449e1d2a4ff3f7d0ca93a951911e9b57380c
Carlos Sandoval <iznomag@gmail.com>
2019-05-16 13:53:15 -0500

    Add fourth exercise

--- script.js
+++ script.js
@@ -45,3 +45,14 @@ function convertHashToArray(hash) {
   }
   return array.sort();
 }
+
+//Deep Freeze
+
+Object.deepFreeze = function(object) {
+  Object.freeze(object);
+  for (var key in object) {
+    if (typeof object[key] === "object") {
+      Object.deepFreeze(object[key]);
+    }
+  }
+};
